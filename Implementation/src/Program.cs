477af6219dd685f27832c993403d0ff963104601
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-09-19 14:54:59 +0300

    Add files via upload

--- Implementation/src/Program.cs
+++ Implementation/src/Program.cs
@@ -1,1 +1,22 @@
+﻿﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2022. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
 
+using System;
+
+namespace bsc
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
