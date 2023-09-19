fbc912254f08938117a43b58cedf4e4d3f3681b8
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2023-09-19 14:40:26 +0300

    Add files via upload

--- Implementation/src/third_party/Mono/Empty.cs
+++ Implementation/src/third_party/Mono/Empty.cs
@@ -1,1 +1,32 @@
+//
+// Author:
+//   Jb Evain (jbevain@gmail.com)
+//
+// Copyright (c) 2008 - 2015 Jb Evain
+// Copyright (c) 2008 - 2011 Novell, Inc.
+//
+// Licensed under the MIT/X11 license.
+//
 
+using System;
+
+namespace Mono
+{
+
+    internal static class Empty<T>
+    {
+
+        public static readonly T[] Array = new T[0];
+
+    }
+
+    internal class ArgumentNullOrEmptyException : ArgumentException
+    {
+
+        public ArgumentNullOrEmptyException(string paramName) : base("Argument null or empty", paramName)
+        {
+        }
+
+    }
+
+}
